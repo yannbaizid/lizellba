23b038258534d1883189b7c944675b859d380f93
yannbaizid <yannbaizid@free.fr>
2021-01-08 12:32:42 +0100

    added closing modal. pop up when you cick on "X"

---
+++ src/game/components/ClosingModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <modal
+      ref="ClosingModal"
+      id="closing_modal"
+      @closeModalEvent="handleCloseModalEvent"
+    >
+      <!--ASK FOR CONFIRMATION -->
+
+      <div class="bold">
+        Es-tu sûr de vouloir quitter le montage sans valider l'exposition?
+      </div>
+
+      <div class="flexbox flexbox_col flexbox_spacearound">
+        <div @click="validateExpo()">
+          <app-button message="Valider votre exposition" />
+        </div>
+        <div @click="closeModal()">
+          <app-button message="Retourner sur le montage" />
+        </div>
+        <div @click="exit()">
+          <app-button message="oui" />
+        </div>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+import AppButton from "../../services/AppButton.vue";
+
+import Modal from "../../services/Modal.vue";
+export default {
+  components: { Modal, AppButton },
+  name: "ClosingModal",
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    openModal() {
+      console.log("Open da shit");
+      this.$refs.ClosingModal.openModal();
+    },
+    handleCloseModalEvent() {
+      console.log("handleclosemodalevent");
+    },
+    closeModal() {
+      this.$refs.ClosingModal.closeModal();
+    },
+    exit() {
+      this.$emit("closeExpoEvent");
+      this.closeModal();
+    },
+    validateExpo() {
+      this.$emit("askExpoValidationEvent");
+      this.closeModal();
+    },
+  },
+};
+</script>
+<style lang="scss">
+</style>
