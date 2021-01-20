14680699433825f3ed399fc0b6232adadd843734
yannbaizid <yannbaizid@free.fr>
2021-01-20 18:55:32 +0100

    loading is now a nice gif! nice!

--- src/services/LoadingModal.vue
+++ src/services/LoadingModal.vue
@@ -1,24 +1,55 @@
 <template>
-  <modal   ref="LoadingModal"
-      id="loading_modal"
- :closable="false"
-      @closeModalEvent="handleCloseModalEvent"
-    ><div class="h_100 flexbox_col flexbox_justifycenter">
-        <div class="font_size_big bolder">LOADING</div>
-        </div>
-    </modal>
+ <div id="loadingModal" class="modal">
+   <div class="modal_backdrop" >
+   </div>
+       <div class="modal_dialog">
+       <img src="..\\assets\loading.gif" alt="logo">
+       </div>
+ </div>
 </template>
 
 <script>
-import Modal from "./Modal.vue";
+
 export default {
-  components: { Modal },
+ 
   mounted() {
       console.log('opening modal loading');
-      this.$refs.LoadingModal.openModal();
+     
   }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+  overflow-x: hidden;
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &_backdrop {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+  &_dialog {
+
+
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    z-index: 2;
+ 
+  }
+}
 </style>
