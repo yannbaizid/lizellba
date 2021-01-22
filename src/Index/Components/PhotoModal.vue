03e90276a0ddd0d95b564a77112fe36faba6160c
yannbaizid <yannbaizid@free.fr>
2021-01-22 17:12:33 +0100

    navigation between photos is set up. Next step on gallery: try to pass displayedPhoto to photoModal by props

--- src/Index/Components/PhotoModal.vue
+++ src/Index/Components/PhotoModal.vue
@@ -11,14 +11,55 @@
           @click.native="closeModal()"
         />
         <div class="flexbox_col flexbox_spacebetween flexbox_alignstart h_100">
-          <div class="flexbox_row flexbox_spacebetween w_100">
-            <div class="float_left" v-if="photo.id<idRange.max" >&#12296;</div>
+          <div class="flexbox_row flexbox_spacebetween w_100 flex_grow">
+            <div
+              id="previous_button"
+               class="navigation_button"
+              v-if="photo.id < idRange.max"
+              @click="onPrevious()"
+            >
+               <svg
+                width="17"
+                height="34"
+                viewBox="0 0 17 34"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path 
+                class="navigation_svg"  transform="rotate(180,9,17)"
+                  d="M1 1L15.6667 17L1 33"
+                  stroke="black"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
             <img
               class="img_rnd photo_modal_img w_100 flex_shrink"
               :src="imgLink + '/expos/' + photo.file_name"
               :alt="photo.img_name"
             />
-            <div v-if="photo.id>idRange.min">></div>
+            <div
+              v-if="photo.id > idRange.min"
+              id="next_button"
+              class="navigation_button"
+              @click="onNext()"
+            >
+              <svg
+                width="17"
+                height="34"
+                viewBox="0 0 17 34"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1L15.6667 17L1 33"
+                  stroke="black"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
           </div>
           <div class="flexbox_row flexbox_spacebetween w_100">
             <div
@@ -58,9 +99,15 @@ export default {
     };
   },
   props: {
-    idRange: Object
+    idRange: Object,
   },
   methods: {
+    onNext() {
+      this.$emit("nextPhotoEvent");
+    },
+    onPrevious() {
+      this.$emit("previousPhotoEvent");
+    },
     closeModal() {
       this.show = false;
       document.querySelector("body").classList.remove("overflow-hidden");
@@ -76,7 +123,6 @@ export default {
   },
   mounted() {
     console.log("photomodal mounted");
-    
   },
 };
 </script>
@@ -125,6 +171,35 @@ export default {
   padding: 5px 0px;
 }
 .photo_modal_img {
-  padding: 0px 20px;
+  margin: 0px 20px;
+  background-color: pink;
+}
+
+.navigation_button {
+  margin: 20px;
+  padding: 10px;
+  border-radius: 2px;
+  background-color: rgba(247, 247, 247, 0.1);
+  stroke: black;
+  &:hover {
+    cursor: pointer;
+    stroke:#9A9A9A;
+     background-color: rgba(252, 252, 252, 0.1);
+  }
+
+  svg {
+    stroke: inherit;
+  }
+  path {
+    stroke: inherit;
+  }
+}
+#previous_button {
+  position: absolute;
+  left: 0px;
+}
+#next_button {
+  position: absolute;
+  right: 0px;
 }
 </style>
