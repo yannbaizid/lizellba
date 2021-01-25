68a77ed3569e4f345ec03f9ef4267e89d132df4a
yannbaizid <yannbaizid@free.fr>
2021-01-25 22:56:30 +0100

    Our name first!

--- src/Index/Index.vue
+++ src/Index/Index.vue
@@ -8,8 +8,8 @@
     </div>
     <div id="index_footer" class="align_left">
       <div id="typo_credits" class="font_size_small text_align_left">
-        Crédit typographique pour le logo Lizellba : Jocelyn Cottencin.<br>
-        Graphisme: Fanny Martel. Développement: Yann Baïzid. Conception additionnelle: Léa Bénétou.
+        Graphisme: Fanny Martel. Développement: Yann Baïzid. Conception additionnelle: Léa Bénétou.<br>
+        Crédit typographique pour le logo Lizellba : Jocelyn Cottencin.
       </div>
     </div>
   </div>
