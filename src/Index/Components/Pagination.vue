07977a32be5257ad2cc1be33be90f4b3466dcd08
yannbaizid <yannbaizid@free.fr>
2021-03-09 13:04:21 +0100

    creating a component to deal with pagination. Takes current page and total page count as props. Display pages number. Emit pageSelectionEvent with target page when a page is selected.

---
+++ src/Index/Components/Pagination.vue
@@ -0,0 +1,182 @@
+<template>
+  <div>
+    <div class="flexbox_row">
+      <div class="flexbox_row flexbox_justifycenter h_100 app_navarrow">
+        <div
+          @click=" handlePageSelection(currentPage-1)"
+          v-if="currentPage > 1"
+          class="flexbox_row clickable"
+        >
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              transform="rotate(180,4, 7)"
+              d="M1 1L6.71429 7L1 13"
+              stroke="black"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    <!--   <div v-if="currentPage > 3" class="app_pagination_item">1</div>
+      <div v-if="currentPage > 4" class="app_pagination_item">
+        {{ pageRange[0] == 3 ? "2" : "..." }}
+      </div> -->
+
+      <div
+        v-for="page in pageRange"
+        :key="page"
+        class="app_pagination_item"
+        :class="page == currentPage ? 'bolder' : '' +  page!='...'? 'clickable': ''"
+
+        @click="page!='...'? handlePageSelection(page) : null"
+      >
+        {{ page }}
+      </div>
+  <!--     <div v-if="pageCount - currentPage > 3" class="app_pagination_item">
+        {{
+          pageRange[pageRange.length - 1] == pageCount - 2
+            ? pageCount - 1
+            : "..."
+        }}
+      </div>
+      <div v-if="pageCount - currentPage > 2" class="app_pagination_item">
+        {{ pageCount }}
+      </div>
+ -->
+      <div class="flexbox_row flexbox_justifycenter h_100 app_navarrow">
+        <div
+          @click=" handlePageSelection(currentPage+1)"
+          v-if="currentPage < pageCount"
+          class="flexbox_row clickable"
+        >
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 1L6.71429 7L1 13"
+              stroke="black"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    currentpage: {{ currentPage }} {{ pageRange }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    pageRange: function () {
+      /*    let pageRangeTotalSize = 2 * this.pageRangeSize + 1;
+      console.log("page rangtotal" + pageRangeTotalSize);
+
+      let pageRange = [];
+      let pageRangeMin = Math.min(
+        this.currentPage,
+        Math.max(
+          1,
+          this.currentPage -
+            this.pageRangeSize -
+            (this.pageCount - this.currentPage < this.pageRangeSize
+              ? this.pageRangeSize - (this.pageCount - this.currentPage)
+              : 0)
+        )
+      );
+      console.log("test");
+      let pageRangeMax = Math.max(
+        this.currentPage,
+        Math.min(
+          this.pageCount,
+          this.currentPage +
+            this.pageRangeSize +
+            (this.currentPage <= this.pageRangeSize
+              ? this.pageRangeSize + 1 - this.currentPage
+              : 0)
+        )
+      );
+      for (let i = pageRangeMin; i <= pageRangeMax; i++) {
+        pageRange.push(i);
+      } */
+      let pageRangeLengthMax = 7;
+      let pageRangeLength= Math.min(pageRangeLengthMax,this.pageCount);
+      console.log('pagelength'+pageRangeLength);
+      let pageRangeMiddle = Math.floor(pageRangeLength / 2);
+      let pageRange = new Array(pageRangeLength);
+      pageRange[0] = 1;
+      pageRange[pageRangeLength - 1] = this.pageCount;
+      if (this.currentPage<=pageRangeMiddle) {
+        console.log("<pageMiddle");
+        for (let i=0;i<pageRangeLength-1;i++) {
+          pageRange[i]=i+1;
+        }
+      }
+     else  if (this.currentPage>=this.pageCount-pageRangeMiddle) {
+        console.log(">pageMiddle");
+       for (let i=0;i<pageRangeLength-1;i++) {
+          pageRange[pageRangeLength-i-1]=this.pageCount-i;
+        }
+      }
+      else {
+         console.log("=pageMiddle");
+        pageRange[pageRangeMiddle]=this.currentPage;
+        for (let i=1;i<pageRangeMiddle;i++) {
+          pageRange[pageRangeMiddle-i]=this.currentPage-i;
+          pageRange[pageRangeMiddle+i]=this.currentPage+i;
+        }
+      }
+      if (this.pageCount-pageRange[pageRangeLength-2]>1) {
+
+        pageRange[pageRangeLength-2]='...';
+      }
+      if (pageRange[1]-pageRange[0]>1) {
+
+        pageRange[1]='...';
+      }
+
+      return pageRange;
+    },
+  },
+  data() {
+    return {
+      pageRangeSize: 2,
+    };
+  },
+  props: {
+    pageCount: Number,
+    currentPage: Number,
+  },
+  methods: {
+    handlePageSelection(page) {
+      this.$emit("pageSelectionEvent",page);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.active {
+  color: red;
+}
+
+.app_pagination_item {
+  width: 1.5rem;
+}
+
+.app_navarrow {
+  width: calc(1rem + 8px);
+}
+</style>
--- src/views/TestModal.vue
+++ src/views/TestModal.vue
@@ -1,34 +1,44 @@
 <template>
-  <tuto-modal />
+  <pagination
+    :currentPage="currentPage"
+    :pageCount="3"
+    @pageSelectionEvent="handlePageSelection"
+  />
 </template>
 
 <script>
-import TutoModal from '../game/components/TutoModal.vue';
+import Pagination from "../Index/Components/Pagination.vue";
 
 export default {
   name: "testmodal",
   data() {
     return {
       select: 1,
-      show: false
+      show: false,
+      currentPage: 4,
     };
   },
   methods: {
+    handlePageSelection(page) {
+      console.log("lol");
+      console.log("page");
+      this.currentPage = page;
+    },
     showModal() {
       this.$refs.testmodal.openModal();
     },
     toggle() {
-        this.select++;
-        if (this.select==4) {
-            this.select=1;
-        }
-        console.log(this.select+" toggle");
-        this.show=!this.show;
-    }
+      this.select++;
+      if (this.select == 4) {
+        this.select = 1;
+      }
+      console.log(this.select + " toggle");
+      this.show = !this.show;
+    },
   },
   components: {
-    TutoModal
-  }
+    Pagination,
+  },
 };
 </script>
 
